7ccb867883476a3aac8e9f985d96a41b970b3f0c
Hamid Shariati <hmdshariati@aol.com>
2025-04-04 02:16:24 +0330

    add skeleton submodule

--- pm4-package-skeleton/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use ProcessMaker\Package\PackageSkeleton\Http\Controllers\PackageSkeletonController;
-
-Route::group(['middleware' => ['auth:api', 'bindings']], function () {
-    Route::get('admin/package-skeleton/fetch', [PackageSkeletonController::class, 'fetch'])->name('package.skeleton.fetch');
-    Route::apiResource('admin/package-skeleton', PackageSkeletonController::class);
-});
--- pm4-package-skeleton/routes/console.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// Console routes
