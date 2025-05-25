e97026da58356e25e8db32c2166a2358bc63a038
Hamid Shariati <hmdshariati@aol.com>
2025-05-25 08:29:29 +0330

    corrections

--- v3php7.1/paths_installed.php
+++
@@ -1,5 +0,0 @@
-<?php
-  define('PATH_DATA',         '/opt/processmaker/shared/');
-  define('PATH_C',            '/opt/processmaker/shared/compiled/');
-  define('HASH_INSTALLATION', 'pdTEwp7Mn6yoyMfEl5eTmm2Um8TImMjGcpJplZaZlZpyxpWVZZhgbm3V1dLZmMTCa5VlmpfIy8ielJeVbGVllpzKxsZrxWRtZ5eWmJ5klZRu');
-  define('SYSTEM_HASH',       'eaa2d094cfce3ca915aceee9a4434059');
